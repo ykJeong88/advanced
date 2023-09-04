2c12d75afa62099318f2c6f220f3a1aadc6985cb
ykjeong <mohargga@gmail.com>
2023-09-04 22:37:11 +0900

    프록시 패턴과 데코레이터 패턴 3. 예제 프로젝트 만들기 v2 4. 예제 프로젝트 만들기 v3

---
+++ src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -0,0 +1,28 @@
+package hello.proxy.app.v3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+
+    public OrderControllerV3(OrderServiceV3 orderServiceV3) {
+        this.orderService = orderServiceV3;
+    }
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
