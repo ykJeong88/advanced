813b528157e923d4a34e60143d6c4edf5ca79b46
ykjeong <mohargga@gmail.com>
2023-09-14 22:41:44 +0900

    동적 프록시 기술 5. JDK 동적 프록시 - 적용2

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,7 @@ import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV2Config.class, AppV1Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
