84faddf90abd2cd9be0c325faf2d54e7afeceba5
ykjeong <mohargga@gmail.com>
2023-09-19 22:41:00 +0900

    스프링이 지원하는 프록시 4. 포인트컷, 어드바이스, 어드바이저 - 소개 5. 예제코드1 - 어드바이저

---
+++ src/test/java/hello/advanced/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,23 @@
+package hello.advanced.proxy.advisor;
+
+import hello.advanced.proxy.common.advice.TimeAdvice;
+import hello.advanced.proxy.common.service.ServiceImpl;
+import hello.advanced.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.save();
+        proxy.find();
+    }
+}
