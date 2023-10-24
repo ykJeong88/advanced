17a14fc6e52467c4e42cb07c013ccd474095eab1
ykjeong <mohargga@gmail.com>
2023-10-24 22:19:06 +0900

    스프링 AOP 구현 5. 스프링 AOP 구현3 - 어드바이스 추가 6. 스프링 AOP 구현4 - 포인트컷 참조 7. 스프링 AOP 구현5 - 어드바이스 순서

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -1,14 +1,16 @@
 package hello.aop;
 
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopApplication {
 
 	public static void main(String[] args) {
